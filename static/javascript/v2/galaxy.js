d13aa6c3409f2f35fee45c32e92222ea29208750
Luke Mallon (Nalum) <luke@mallon.ie>
2021-09-29 22:25:43 +0100

    Adding comments for others to follow

--- static/javascript/v2/galaxy.js
+++ static/javascript/v2/galaxy.js
@@ -42,6 +42,8 @@ const minStarSize = new paper.Size(2,2);
 var scale = 7e-16;
 const minZoom = 7e-16;
 const maxZoom = 0.00012368155244843137;
+
+// Messing around trying to figure out the zoom issue
 var center = new paper.Point(0,0);
 center.radial_distance = 0;
 center.longitude = 0;
@@ -52,6 +54,7 @@ center.calculatePosition = function() {
 	);
 	return point;
 };
+
 // Offset will be used to allow panning around
 var galacticCenter = new paper.Point(0,0);
 // This value allows us to set the alpha of the star based on it's luminosity
@@ -69,7 +72,9 @@ window.addEventListener("load", function() {
 	paper.setup(galaxy);
 
 	with (paper) {
+		// Setup the view so that 0,0 is in the center of the canvas image
 		view.translate(galaxy.width / 2, galaxy.height / 2);
+		// Highlight the center so it's easier to see
 		var centerCircle = Path.Ellipse({
 			center: galacticCenter,
 			radius: 10,
@@ -85,15 +90,19 @@ window.addEventListener("load", function() {
 		};
 
 		view.onMouseDrag = function(event) {
+			// Unset currentStar when dragging to allow dragging to work
 			currentStar = null;
 			galacticCenter = galacticCenter.add(event.delta);
 		};
 
 		view.onFrame = function(event) {
+			// If currentStar is set to a star we force galacticCenter to be offset from that point
 			if (currentStar) {
 				galacticCenter = galacticCenter.subtract(currentStar.calculatePosition());
 			}
 
+			// Assuming we have Stars we want to have them added back into the image if they were removed
+			// due to being out of bounds.
 			if (typeof stars !== "undefined") {
 				Object.keys(stars).forEach(function(starID) {
 					var pos = stars[starID].calculatePosition();
@@ -150,18 +159,25 @@ var req = new XMLHttpRequest();
 req.overrideMimeType("application/json");
 req.open('GET', '/stars', true);
 
+// Process the Star data on successful load
 req.addEventListener("load", function () {
 	if (req.readyState == 4 && req.status == "200") {
 		stars = JSON.parse(req.responseText);
 
+		// Loop over the star data so that we can build the image.
 		Object.keys(stars).forEach(function (starID) {
+			// Getting the max brightness for use in calculating the alpha of the star
 			if (stars[starID].luminosity > maxLums) {
 				maxLums = stars[starID].luminosity;
 			}
 
+			// Add up the total number of planets
 			planetTotal += stars[starID].planets.length;
+			// Add up the total number of stars
 			starTotal += 1;
 
+			// calculatePosition is used to position the Star correctly relative to the
+			// center of the galaxy and the center of the canvas.
 			stars[starID].calculatePosition = function() {
 				var point = new paper.Point(
 					((this.radial_distance * scales.Parsec) * scale) * Math.sin(-this.longitude * (Math.PI / 180)) + galacticCenter.x,
@@ -170,17 +186,22 @@ req.addEventListener("load", function () {
 				return point;
 			};
 
+			// calculateSize uses the radius of the star and multiplies it by the known radius of the sun (Sol).
+			// It is also multiplied by the scale value to scale the star correctly.
+			// minStarSize is returned if the Stars size is less than it at any given scale.
 			stars[starID].calculateSize = function() {
 				var radius = (scales.RSol * this.radius) * scale;
 				var size = new paper.Size(radius, radius);
 				return size.width > minStarSize.width ? size : minStarSize;
 			};
 
+			// Here we are adding the Star to the canvas
 			stars[starID].item = new paper.Path.Ellipse({
 				center: stars[starID].calculatePosition(),
 				radius: stars[starID].calculateSize()
 			});
 
+			// This is calculating the stars color depending on whether there is more than 1
 			if (stars[starID].color.length > 1) {
 				stars[starID].item.fillColor = {
 					gradient: {
@@ -194,10 +215,13 @@ req.addEventListener("load", function () {
 				stars[starID].item.fillColor = stars[starID].color;
 			}
 
+			// For now if you click on the star it will log the ID to the console.
+			// Want to do more here with selection/info/planet display
 			stars[starID].item.onMouseUp = function() {
 				console.log(this.star.star_id);
 			};
 
+			// Do more here but also need to increase the hitbox size to allow easier clicking
 			stars[starID].item.onMouseEnter = function() {
 				galaxy.className = "pointer";
 			};
@@ -206,29 +230,40 @@ req.addEventListener("load", function () {
 				galaxy.className = "";
 			};
 
+			// onFrame is called 60 times a second (where possible) and is used to resize and
+			// recalculate the position of the stars when the map is dragged or zoomed.
 			stars[starID].item.onFrame = function(event) {
+				// Update the Star position
 				var pos = this.star.calculatePosition();
 				this.setPosition(pos);
+				// Calculate the Star bounds
 				var bounds = new paper.Rectangle({
 					center: pos,
 					size: this.star.calculateSize()
 				});
 
+				// If the bounds have changed let's apply them.
 				if (!this.bounds.equals(bounds)) {
 					this.set({bounds: bounds});
 				}
 
+				// If the Star is no longer in the bounds of the canvas view let's
+				// remove it to reduce the required resources.
 				if (!this.isInside(paper.view.bounds)) {
 					this.remove();
 				} else {
+					// If the Star is still in the bounds we want to check the current scale
+					// and start showing the planets and their orbits around the star
 					if (scale > 9.6626212850337e-7) {
-						var that = this;
+						var that = this; // refernce the Star
 						stars[this.star.star_id].planets.forEach(function(planet) {
+							// So long as there is no parent we want to add the Planets Orbit to that
 							if (planet.orbit.parent === null) {
 								planet.orbit.addTo(that);
 								console.log(planet);
 							}
 
+							// So long as there is no parent we want to add the Planet to that
 							if (planet.item.parent === null) {
 								planet.item.addTo(that);
 								console.log(planet);
@@ -238,28 +273,42 @@ req.addEventListener("load", function () {
 				}
 			};
 
+			// Add the Star as a reference to item
 			stars[starID].item.star = stars[starID];
 
+			// Loop over each Planet and set them up for later use.
+			// Might be a better way of doing this.
 			stars[starID].planets.forEach(function(planet) {
+				// Parse the axis to a float as it is currently a string
 				var semimajor_axis = isNaN(parseFloat(planet.semimajor_axis)) ? 0.01 : parseFloat(planet.semimajor_axis);
+				// Calculate the Planet Radius with current scale
 				var radius = planetSizes[planet.size] * scale;
+				// Create a size for later use
 				var size = new paper.Size(radius, radius);
+				// Calculate the Planets position
 				var pos = stars[starID].item.position + new paper.Point((((scales.AU * semimajor_axis) * scale) * Math.LOG10E), 0);
+				// Setup an orbit and assign it to the Planet
 				planet.orbit = new paper.Path.Ellipse({
 					center: stars[starID].item.position,
 					radius: stars[starID].calculateSize() + size + (((scales.AU * semimajor_axis) * scale) * Math.LOG10E),
 					strokeColor: planet.color,
 					stokeWidth: 1
 				});
+				// Reference the Planet in the Orbit
+				planet.orbit.planet = planet;
+				// Setup the Planet on the orbit line
 				planet.item = new paper.Path.Ellipse({
 					center: pos,
 					radius: size,
 					fillColor: planet.color
 				});
-				planet.orbit.planet = planet;
+				// Reference the Planet
 				planet.item.planet = planet;
+				// Remove the Orbit and Planet from the image as we are zoomed out and cannot see them anyway.
 				planet.orbit.remove();
 				planet.item.remove();
+				
+				// V1 Calculations for reference.
 				// var x = ((systemView.AU * semimajor_axis) * Math.LOG10E) + starSize + radius;
 				// drawOrbit(ctx, x, radius/2, planet.color);
 				// drawPlanet(ctx, x, 0, radius, planet, typeof planet.selected === "undefined" && i===systemItem ? true : planet.selected);
@@ -271,6 +320,7 @@ req.addEventListener("load", function () {
 		populateSelects();
 		document.getElementById("start").disabled = false;
 		document.getElementById("start").innerHTML = "Start";
+		// Hardcode an initial focus while working out the zoom movement issue
 		currentStar = stars[330];
 	}
 });
